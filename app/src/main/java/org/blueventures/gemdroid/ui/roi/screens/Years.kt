cc65d921dcf31f8e85d4965effc3060735e24593
zibnix <cfexit@protonmail.com>
2023-11-13 11:19:18 -0800

    tweak wording in a couple places

--- app/src/main/java/org/blueventures/gemdroid/ui/roi/screens/Years.kt
+++ app/src/main/java/org/blueventures/gemdroid/ui/roi/screens/Years.kt
@@ -9,7 +9,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -72,7 +72,7 @@ object Years {
 
     @Composable
     fun SelectYear(initYear: Int, currentYear: Int, setYear: (Int) -> Unit) {
-        var year by remember { mutableStateOf(initYear) }
+        var year by remember { mutableIntStateOf(initYear) }
         NumberPicker(
             value = year,
             range = RoiViewModel.oldestLandsatYear..currentYear,
